2dc10366576498658a4def5a91b463e02ec588b2
Gabriel Teles <93494655+DevM4yCry@users.noreply.github.com>
2021-11-01 20:26:28 -0300

    40 minutes watched

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
+#Youtube Video [https://youtu.be/t8pPdKYpowI]
+
+#IDE = Integrated Developmentee Environment
+# Print(int)
+print("INTEGER NUMBER:")
+print(100)
+
+# Print(float)
+print("FLOAT NUMBER:")
+print(100.01)
+
+# Print("string")
+print("STRING CARAC:")
 print("Hello World")
+
+#calculation of minutes for 20 days
+print("20 days in seconds are:")
+print(20 * 24 * 60)
+
+#string concatenation
+print("20 days are " + str(20*24*60) + " seconds")
+
+#this string concatenation only works on phyton 3 or superior
+print(f"30 days are {30*24*60} seconds")
+
+#using Variables => name = value
+##type1
+var_1 = 40*24*60
+und = "seconds"
+print(f"40 days are {var_1} {und}")
+
+##type2
+var_2 = 50*24*60
+und = "seconds"
+print("50 days are " + str(var_2) + " " + (und))
+
+##  There's some keywords that are reserved in Python:
+##  and, as, assert, break, class, continue, def, del, elif, else
+##  except, finally, false, for, from, global, if, import, in, is, lambda, nonlocal, none
+##  not, or, pass, raise, return, true, try, with, while, yield
+
+#functions
+
+
+
